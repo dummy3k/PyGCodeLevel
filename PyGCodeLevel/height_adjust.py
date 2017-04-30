f762d0e2caebdc14ee5002be05235241656f4a1c
dummy3k <l4711@gmx.net>
2017-04-30 13:59:58 +0200

    refactor, might broken somethings

--- PyGCodeLevel/height_adjust.py
+++ PyGCodeLevel/height_adjust.py
@@ -13,6 +13,8 @@ import argparse
 import matplotlib
 import matplotlib.pyplot as plt
 
+from gcode import gcode_file_info, match_movement, daniel
+
 short_sleep = 0.1
 long_sleep = 1
 
@@ -62,51 +64,10 @@ def wait_idle(s):
 		else:
 			return r
 			
-def match_movement(s):
-	"""
-		>>> match_movement("X")
-		(False, None, None)
-		
-		>>> match_movement("G01 X1.0500Y-27.8130")
-		(True, 'G01', [1.05, -27.813, None])
-		
-		>>> match_movement("G01 Z-0.2000")
-		(True, 'G01', [None, None, -0.2])
-		
-		>>> match_movement("G00 X39.3197Y-20.3092")
-		(True, 'G00', [39.3197, -20.3092, None])
-	"""
-	m = re.match(r'G0\d', s)
-	if not m:
-		return (False, None, None)
-	g = m.group(0)
-	
-	r = [None, None, None]
-	for axis, value in re.findall(r"([XYZ])([-\d\.]+)", s):
-		r[ord(axis) - ord('X')] = float(value)
-
-	return (True, g, r)
 		
 if __name__ == '__main__':
 	helper.logging_config()
 
-def daniel(A, B, C, D, a1, a2, b1, b2):
-	"""
-		>>> daniel(1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5)
-		1.0
-		
-		>>> daniel(1, 2, 3, 4, 0, 1, 0, 1)
-		1.0
-		
-		>>> daniel(1, 2, 3, 4, 1, 0, 1, 0)
-		4.0
-	"""
-	if a1 + a2 != 1 or b1 + b2 != 1:
-		raise Exception("a/b !=1")
-		
-	return (1.0 - a1) * ((1.0 - b1) * A + (1.0 - b2) * B) \
-	     + (1.0 - a2) * ((1.0 - b1) * C + (1.0 - b2) * D)
-		
 	
 def multiplex_files(gcode_filename, bounds, heightmap_filename, step_size=5):
 	# G21
@@ -189,43 +150,6 @@ def multiplex_files(gcode_filename, bounds, heightmap_filename, step_size=5):
 				# G94
 				print(line)
 
-def gcode_file_info(gcode_filename):
-	min_x = 0
-	min_y = 0
-	max_x = 0
-	max_y = 0
-	
-	# G01 X10Y10Z10
-	# G01 X00Y00Z00
-	points = []
-	points3d = []
-	
-	with open(gcode_filename, 'r') as gcode_f:
-		for line in gcode_f:
-			line = line.strip()
-			g, success, vector = match_movement(line)
-			if success:
-				if vector[0] and vector[0] < min_x:
-					min_x = vector[0]
-				if vector[0] and vector[0] > max_x:
-					max_x = vector[0]
-					
-				if vector[1] and vector[1] < min_y:
-					min_y = vector[1]
-				if vector[1] and vector[1] > max_y:
-					max_y = vector[1]
-				
-				if vector[0] and vector[1]:
-					points.append(vector[:2])
-					
-				if vector[0] and vector[1] and vector[2] and vector[2] < 0:
-					points3d.append(vector)
-				
-	r = (min_x, min_y, max_x, max_y)
-	#print("(%s, %s), (%s, %s)" % r)
-	# print(points)
-	# print(points3d)
-	return r
 	
 def sprint_hmap(hm):
     print ("[ "),
